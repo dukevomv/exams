cb0bfc074b9b174cac0966439bdd61501ece1d1f
Duke Vomvyras <dukevomv@gmail.com>
2017-09-17 21:43:32 +0300

    create segment task basic animations added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,98 +1,101 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type='text/css'></link>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <!-- Styles -->
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type='text/css'></link>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+  <div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        @if (Auth::guest())
-                            <li>
-                                <a href="{{ url('/home') }}">Home</a>
-                            </li>
-                        @elseif(Auth::user()->role == 'professor')
-                            <li class="{{ Request::is('lessons') || Request::is('lessons/*') ? 'active' : '' }}">
-                                <a href="{{ url('/lessons') }}">Lessons</a>
-                            </li>
-                            <li class="hidden {{ Request::is('tests') || Request::is('tests/*') ? 'active' : '' }}">
-                                <a href="{{ url('/tests') }}">Tests</a>
-                            </li>
-                            <li class="{{ Request::is('segments') || Request::is('segments/*') ? 'active' : '' }}">
-                                <a href="{{ url('/segments') }}">Segments</a>
-                            </li>
-                        @elseif(Auth::user()->role == 'student')
-                            
-                        @endif
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                   <span class="label label-primary">{{ucfirst(Auth::user()->role)}}</span> {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="{{url('/settings')}}"><i class="fa fa-cog"></i> Settings</a></li>
-                                    <li role="separator" class="divider"></li>
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            <i class="fa fa-sign-out"></i> Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
             </div>
-        </nav>
-        @yield('content')
-    </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    @if (Auth::guest())
+                        <li>
+                            <a href="{{ url('/home') }}">Home</a>
+                        </li>
+                    @elseif(Auth::user()->role == 'professor')
+                        <li class="{{ Request::is('lessons') || Request::is('lessons/*') ? 'active' : '' }}">
+                            <a href="{{ url('/lessons') }}">Lessons</a>
+                        </li>
+                        <li class="hidden {{ Request::is('tests') || Request::is('tests/*') ? 'active' : '' }}">
+                            <a href="{{ url('/tests') }}">Tests</a>
+                        </li>
+                        <li class="{{ Request::is('segments') || Request::is('segments/*') ? 'active' : '' }}">
+                            <a href="{{ url('/segments') }}">Segments</a>
+                        </li>
+                    @elseif(Auth::user()->role == 'student')
+                        
+                    @endif
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                               <span class="label label-primary">{{ucfirst(Auth::user()->role)}}</span> {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{url('/settings')}}"><i class="fa fa-cog"></i> Settings</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        <i class="fa fa-sign-out"></i> Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+  </div>
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}"></script>
+
+  @yield('scripts')
+        
 </body>
 </html>
--- resources/views/segments/create.blade.php
+++ resources/views/segments/create.blade.php
@@ -3,13 +3,13 @@
 @section('content')
   <div class="container">
     <div class="row">
-      <div class="col-md-8 col-md-offset-3">
+      <div class="col-md-8 col-md-offset-3" id="segment-body">
         <h1>Create Segment</h1>
         <div class="panel panel-default relative">
           <div class="order-wrap disabled">
-            <div class="order-up cursor-pointer"><i class="fa fa-angle-up" aria-hidden="true"></i></div>
+            <div class="order-trigger cursor-pointer" data-order-direction="up"><i class="fa fa-angle-up" aria-hidden="true"></i></div>
             <div class="order-value">0</div>
-            <div class="order-down cursor-pointer"><i class="fa fa-angle-down" aria-hidden="true"></i></div>
+            <div class="order-trigger cursor-pointer" data-order-direction="down"><i class="fa fa-angle-down" aria-hidden="true"></i></div>
           </div>
           <div class="panel-heading">Basic Information</div>
           <div class="panel-body">
@@ -36,94 +36,101 @@
             </div>
           </div>
         </div>
-        <div class="panel panel-default relative">
-          <div class="order-wrap">
-            <div class="order-up cursor-pointer"><i class="fa fa-angle-up" aria-hidden="true"></i></div>
-            <div class="order-value">1</div>
-            <div class="order-down cursor-pointer"><i class="fa fa-angle-down" aria-hidden="true"></i></div>
-          </div>
-          <div class="panel-heading">Single Choice Task <span class="trash-btn pull-right"><i class="fa fa-trash"></i></span></div>
-          <div class="panel-body">
-            <div class="col-md-12 row-margin-bottom">
-              <label>Task Title:</label>
-              <textarea type="text" class="form-control" placeholder="What is Bootstrap?"></textarea>
-            </div>
-            <div class="col-md-12 task-answer row-margin-bottom">
-              <div class="input-group">
-                <span class="input-group-addon cursor-pointer"><i class="fa fa-arrows" aria-hidden="true"></i></span>
-                <input type="text" name="answers[0]" class="form-control" placeholder="Choice 1">
-                <span class="input-group-addon">
-                  <label class="cursor-pointer"><input type="radio" name="correct" checked> Correct</label>
-                </span>
-              </div>
-            </div>
-            <div class="col-md-12 task-answer row-margin-bottom">
-              <div class="input-group">
-                <span class="input-group-addon cursor-pointer"><i class="fa fa-arrows" aria-hidden="true"></i></span>
-                <input type="text" name="answers[0]" class="form-control" placeholder="Choice 2">
-                <span class="input-group-addon">
-                  <label class="cursor-pointer"><input type="radio" name="correct"> Correct</label>
-                </span>
-              </div>
-            </div>
-            <div class="col-md-12">
-              <button type="button" class="btn btn-link" >
-                <i class="fa fa-plus"></i> Add Choice
-              </button>
-            </div>
-          </div>
-        </div>
-        <div class="panel panel-default relative">
-          <div class="order-wrap">
-            <div class="order-up cursor-pointer"><i class="fa fa-angle-up" aria-hidden="true"></i></div>
-            <div class="order-value">2</div>
-            <div class="order-down cursor-pointer"><i class="fa fa-angle-down" aria-hidden="true"></i></div>
-          </div>
-          <div class="panel-heading">Multiple Choice Task <span class="trash-btn pull-right"><i class="fa fa-trash"></i></span></div>
-          <div class="panel-body">
-            <div class="col-md-12 row-margin-bottom">
-              <label>Task Title:</label>
-              <textarea type="text" class="form-control" placeholder="What is Bootstrap?"></textarea>
-            </div>
-            <div class="col-md-12 task-answer row-margin-bottom">
-              <div class="input-group">
-                <span class="input-group-addon cursor-pointer"><i class="fa fa-arrows" aria-hidden="true"></i></span>
-                <input type="text" name="answers[0]" class="form-control" placeholder="Choice 1">
-                <span class="input-group-addon">
-                  <label class="cursor-pointer"><input type="checkbox" name="correct" checked> Correct</label>
-                </span>
-              </div>
-            </div>
-            <div class="col-md-12 task-answer row-margin-bottom">
-              <div class="input-group">
-                <span class="input-group-addon cursor-pointer"><i class="fa fa-arrows" aria-hidden="true"></i></span>
-                <input type="text" name="answers[0]" class="form-control" placeholder="Choice 2">
-                <span class="input-group-addon">
-                  <label class="cursor-pointer"><input type="checkbox" name="correct"> Correct</label>
-                </span>
-              </div>
-            </div>
-            <div class="col-md-12">
-              <button type="button" class="btn btn-link" >
-                <i class="fa fa-plus"></i> Add Choice
-              </button>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
     <div class="fixed-toolbar col-xs-2">
       <h5>Tools</h5>
       <div class="btn-group margin-bottom-15">
-        <button type="button" class="btn btn-default">
+        <button type="button" class="btn btn-default task-type" data-task-type="rmc">
           <i class="fa fa-plus"></i> Single Choice
         </button>
       </div>
       <div class="btn-group margin-bottom-15">
-        <button type="button" class="btn btn-default">
+        <button type="button" class="btn btn-default task-type" data-task-type="cmc">
           <i class="fa fa-plus"></i> Multiple Choice
         </button>
       </div>
+      <div class="hidden" id="tool-dom">
+        <div class="task-dom" id="rmc-dom">
+          <div class="panel panel-default task-wrap relative">
+            <div class="order-wrap">
+              <div class="order-trigger cursor-pointer" data-order-direction="up"><i class="fa fa-angle-up" aria-hidden="true"></i></div>
+              <div class="order-value"></div>
+              <div class="order-trigger cursor-pointer" data-order-direction="down"><i class="fa fa-angle-down" aria-hidden="true"></i></div>
+            </div>
+            <div class="panel-heading">Single Choice Task <span class="trash-btn pull-right"><i class="fa fa-trash"></i></span></div>
+            <div class="panel-body">
+              <div class="col-md-12 row-margin-bottom">
+                <label>Task Title:</label>
+                <textarea type="text" class="form-control" placeholder="What is Bootstrap?"></textarea>
+              </div>
+              <div class="col-md-12 task-answer row-margin-bottom">
+                <div class="input-group">
+                  <span class="input-group-addon cursor-pointer"><i class="fa fa-arrows" aria-hidden="true"></i></span>
+                  <input type="text" name="answers[0]" class="form-control" placeholder="Choice 1">
+                  <span class="input-group-addon">
+                    <label class="cursor-pointer"><input type="radio" name="correct" checked> Correct</label>
+                  </span>
+                </div>
+              </div>
+              <div class="col-md-12 task-answer row-margin-bottom">
+                <div class="input-group">
+                  <span class="input-group-addon cursor-pointer"><i class="fa fa-arrows" aria-hidden="true"></i></span>
+                  <input type="text" name="answers[0]" class="form-control" placeholder="Choice 2">
+                  <span class="input-group-addon">
+                    <label class="cursor-pointer"><input type="radio" name="correct"> Correct</label>
+                  </span>
+                </div>
+              </div>
+              <div class="col-md-12">
+                <button type="button" class="btn btn-link" >
+                  <i class="fa fa-plus"></i> Add Choice
+                </button>
+              </div>
+            </div>
+          </div> 
+        </div>
+        <div class="task-dom" id="cmc-dom">
+          <div class="panel panel-default task-wrap relative">
+            <div class="order-wrap">
+              <div class="order-trigger cursor-pointer" data-order-direction="up"><i class="fa fa-angle-up" aria-hidden="true"></i></div>
+              <div class="order-value"></div>
+              <div class="order-trigger cursor-pointer" data-order-direction="down"><i class="fa fa-angle-down" aria-hidden="true"></i></div>
+            </div>
+            <div class="panel-heading">Multiple Choice Task <span class="trash-btn pull-right"><i class="fa fa-trash"></i></span></div>
+            <div class="panel-body">
+              <div class="col-md-12 row-margin-bottom">
+                <label>Task Title:</label>
+                <textarea type="text" class="form-control" placeholder="What is Bootstrap?"></textarea>
+              </div>
+              <div class="col-md-12 task-answer row-margin-bottom">
+                <div class="input-group">
+                  <span class="input-group-addon cursor-pointer"><i class="fa fa-arrows" aria-hidden="true"></i></span>
+                  <input type="text" name="answers[0]" class="form-control" placeholder="Choice 1">
+                  <span class="input-group-addon">
+                    <label class="cursor-pointer"><input type="checkbox" name="correct" checked> Correct</label>
+                  </span>
+                </div>
+              </div>
+              <div class="col-md-12 task-answer row-margin-bottom">
+                <div class="input-group">
+                  <span class="input-group-addon cursor-pointer"><i class="fa fa-arrows" aria-hidden="true"></i></span>
+                  <input type="text" name="answers[0]" class="form-control" placeholder="Choice 2">
+                  <span class="input-group-addon">
+                    <label class="cursor-pointer"><input type="checkbox" name="correct"> Correct</label>
+                  </span>
+                </div>
+              </div>
+              <div class="col-md-12">
+                <button type="button" class="btn btn-link" >
+                  <i class="fa fa-plus"></i> Add Choice
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <h5>Actions</h5>
       <div class="btn-group margin-bottom-15">
         <button type="button" class="btn btn-default">
@@ -137,4 +144,44 @@
       </div>
     </div>
   </div>
+@endsection
+
+@section('scripts')
+  <script type="text/javascript">
+
+    $('.fixed-toolbar button.task-type').on('click',function(e){
+      $('#segment-body').append($('#tool-dom #'+$(this).attr('data-task-type')+'-dom').html())
+      ReorderSegmentTasks()
+    })
+
+    $(document).on('click','#segment-body .task-wrap .order-wrap .order-trigger',function(e){
+      let direction = $(this).attr('data-order-direction')
+      let current_task = $(this).closest('.task-wrap')
+      let target_task = null
+      if(direction == 'up')
+        target_task = current_task.prev('.task-wrap')
+      else if(direction == 'down')
+        target_task = current_task.next('.task-wrap')
+
+      if(target_task.length > 0){
+        current_task.detach()
+        if(direction == 'up')
+          current_task.insertBefore(target_task)
+        else if(direction == 'down')
+          current_task.insertAfter(target_task)
+        ReorderSegmentTasks()
+      }
+    })
+
+    $(document).on('click','.task-wrap .trash-btn',function(e){
+      $(this).closest('.task-wrap').remove()
+      ReorderSegmentTasks()
+    })
+
+    function ReorderSegmentTasks(){
+      $("#segment-body .task-wrap").each(function(index) {
+        $(this).find('.order-wrap .order-value').text(index+1)
+      });
+    }
+  </script>
 @endsection
