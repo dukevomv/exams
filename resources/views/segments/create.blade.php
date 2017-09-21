31a3b119df82cab12596e67233edd2e5be423226
Duke Vomvyras <dukevomv@gmail.com>
2017-09-22 01:53:19 +0300

    segment object creation on baseic actions

--- resources/views/segments/create.blade.php
+++ resources/views/segments/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
       <div class="col-md-8 col-md-offset-3" id="segment-body">
         <h1>Create Segment</h1>
-        <div class="panel panel-default relative">
+        <div class="panel panel-default basics-wrap relative">
           <div class="order-wrap disabled">
             <div class="order-trigger cursor-pointer" data-order-direction="up"><i class="fa fa-angle-up" aria-hidden="true"></i></div>
             <div class="order-value">0</div>
@@ -15,24 +15,32 @@
           <div class="panel-body">
             <div class="col-md-2 row-margin-bottom">
               <label>Lesson:</label>
-              <div class="btn-group">
+              <div class="btn-group dropdown-custom">
                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Select Lesson <span class="caret"></span>
                 </button>
                 <ul class="dropdown-menu">
+                  <li class="active dropdown-value-default"><a href="#" data-dropdown-value="default">Select Lesson</a></li>
                   @foreach($lessons as $lesson)
-                    <li @if(Request::input('lesson','') == $lesson->id)class="active"@endif><a href="#">{{$lesson->name}}</a></li>
+                    <li><a href="#" data-dropdown-value="{{$lesson->id}}">{{$lesson->name}}</a></li>
                   @endforeach
                 </ul>
+                <select class="hidden dropdown-select" id="segment-lesson">
+                  <option value="default">Select Lesson</option>
+                  @foreach($lessons as $lesson)
+                    <option value="{{$lesson->id}}">{{$lesson->name}}</option>
+                  @endforeach
+                </select>
               </div>
             </div>
+            <input type="hidden" id="segment-id">
             <div class="col-md-8 col-md-offset-2 row-margin-bottom">
               <label>Name:</label>
-              <input type="text" class="form-control" placeholder="Basic HTML questions">
+              <input type="text" class="form-control" id="segment-name" placeholder="Basic HTML questions">
             </div>
             <div class="col-md-12">
               <label>Description:</label>
-              <textarea type="text" class="form-control" placeholder="All you need to know about HTML..."></textarea>
+              <textarea type="text" class="form-control" id="segment-description" placeholder="All you need to know about HTML..."></textarea>
             </div>
           </div>
         </div>
@@ -138,7 +146,7 @@
         </button>
       </div>
       <div class="btn-group margin-bottom-15">
-        <button type="button" class="btn btn-primary">
+        <button type="button" id="save-btn" class="btn btn-primary">
           <i class="fa fa-save"></i> Save
         </button>
       </div>
@@ -183,11 +191,50 @@
     function ReorderSegmentTasks(){
       $("#segment-body .task-wrap").each(function(index) {
         $(this).find('.order-wrap .order-value').text(index+1)
-      });
+      })
     }
 
     function FocusTask(element){
       element.find('.default-focus').focus()
     }
+
+    $("#save-btn").on('click',function(e){
+      let segment = new Segment()
+      console.log(segment)
+    })
+
+    var Segment = function Segment(){
+      this.constructor
+      this.segment_id  = $('.basics-wrap #segment-id').val().trim() != '' ? $('.basics-wrap #segment-name').val().trim() : null
+      this.lesson_id   = $('.basics-wrap #segment-lesson').val() != 'default' ? parseInt($('.basics-wrap #segment-lesson').val()) : null
+      this.name        = $('.basics-wrap #segment-name').val().trim()
+      this.description = $('.basics-wrap #segment-description').val().trim()
+      this.tasks       = []
+    }
+
+    Segment.prototype.Validate = function(){
+      return false
+    }
+
+    Segment.prototype.UpdateTasks = function(){
+      return false
+    }
+
+    Segment.prototype.UpdateBasics = function(){
+      return false
+    }
+
+    $('.dropdown-custom .dropdown-menu li > a').click(function(e){
+      e.preventDefault()
+      const iconDOM = '<span class="caret"></span>'
+      const value   = $(this).attr('data-dropdown-value')
+      const valueUI = $(this).text()
+      $(this).closest('li').addClass('preactive')
+      let parent    = $(this).closest('.dropdown-custom')
+      parent.find('.dropdown-menu li.active').removeClass('active')
+      parent.find('.dropdown-menu li.preactive').removeClass('preactive').addClass('active')
+      parent.find('.dropdown-toggle').html(valueUI+' '+iconDOM)
+      parent.find('.dropdown-select').val(value)
+    })
   </script>
 @endsection
