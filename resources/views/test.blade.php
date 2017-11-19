8a05ffc8a5bec9de226edff4b9607526b1b3e080
Duke Vomvyras <dukevomv@gmail.com>
2017-11-19 22:00:16 +0200

    code execution added to test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -19,9 +19,16 @@
 
 @section('content')
   <div class="container">
-    <textarea class="col-xs-5 col-md-4" name="" id="execute" >return "I'm dynamic!"</textarea>
-    <div class="col-xs-2 col-md-1 text-center"><button id="run">Run</button></div>
+    <h2>Javascript</h2>
+    <p>Na γραφτεί αλγόριθμος που δέχεται οποιοδήποτε <code>String</code> και επιστρέφει το αντίστροφο του.</p>
+    <div class="col-xs-5 col-md-4">
+      <input class="col-xs-12" placeholder="input" value="Hello World!" id="input" />
+      <textarea class="col-xs-12" name=""  id="execute" >function(x){return x;}</textarea>
+    </div>
+    <div class="col-xs-2 col-md-1 text-center"><button id="run">Test</button></div>
     <div class="col-xs-5 col-md-4" id="results">Press "Run" to see results.</div>
+
+    <div class="col-xs-1" id="valid"></div>
   </div>
 @endsection
 
@@ -29,12 +36,30 @@
   <script>
     $('#run').on('click',function(){
       let script = $('#execute').val()
-      newScript = document.createElement('script');
-      newScript.id = 'scriptContainer';
-      newScript.text = script;
-      let results = new Function(script)();
-      //let results = new eval('({'+script+'})')
-      $('#results').html(results);
+      let results = eval('('+script+')')
+      $('#results').html(results($('#input').val()));
+      Evaluate(results)
     })
+
+    function Rule(x){
+      let y = '';
+      for(let i = x.length - 1;i >= 0; i--){
+        y +=  x[i];
+      }
+      return y;
+    }
+
+    function Evaluate(client_code){
+      console.log(client_code)
+      const y = 'me lene 2';
+      let correct = Rule(y)
+      let pending = client_code(y)
+      console.log(y,correct,pending)
+      if(correct == pending){
+        $('#valid').html('right');
+      } else {
+        $('#valid').html('wrong');
+      }
+    }
   </script>
 @endsection
