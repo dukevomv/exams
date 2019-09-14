2aa62d16370108bf85f07f3e9b5ba055a76eca4c
Duke Vomvyras <dukevomv@gmail.com>
2019-09-14 23:09:32 +0300

    Add code types WIP

---
+++ resources/views/includes/preview/segments/task_types/code.blade.php
@@ -0,0 +1,7 @@
+<div class="panel panel-default task-wrap relative" data-task-type="free-text">
+  <div class="panel-heading">{{$task->description}} <span class="pull-right">{{$task->points}} pts</span></div>
+  <div class="panel-body">
+    <textarea class="col-md-12" rows="7">
+    </textarea>
+  </div>
+</div>
