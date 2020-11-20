ea3977450ab36e43b79ed9c325efc4c19b8cea75
Duke Vomvyras <dukevomv@gmail.com>
2020-11-20 02:31:24 +0200

    [feat] Create UI and functionality for correspondence task type (student view)

---
+++ resources/assets/js/test/task_types/corerspondence.js
@@ -0,0 +1,61 @@
+const taskHandlerIdPrefix = ".task-wrap.task-wrap-correspondence#panel-task-"
+
+function getCorrespondenceTaskAnswersAndElements(taskId) {
+  let task = $(taskHandlerIdPrefix + taskId);
+  let answers = {};
+  task.find('.choice-wrap').each(function () {
+    answers[$(this).find('input.side-a').val()] = {
+      value: $(this).find('input.side-b').val(),
+      element: $(this).find('input.side-b'),
+    }
+  });
+  return answers;
+}
+
+$(".choice-side-b a").click(function (e) {
+  e.preventDefault()
+  const sideB = $(this).text();
+  const parent = $(this).closest('.input-group');
+  const sideA = parent.find('input.side-a').val();
+  const taskId = parent.closest('.task-wrap').attr('data-task-id');
+
+  let taskAnswersAndElements = getCorrespondenceTaskAnswersAndElements(taskId);
+  let taskAnswers = {};
+  Object.keys(taskAnswersAndElements).forEach(a => {
+    //todo this will also replace the newly added!!!
+    if (taskAnswersAndElements[a].value === sideB) {
+      taskAnswersAndElements[a].element.val('');
+    }
+    taskAnswers[a] = taskAnswersAndElements[a].value;
+  });
+  parent.find('input.side-b').val(sideB);
+  taskAnswers[sideA] = sideB
+
+  fixSelectedOptionsInDropdown(taskId,taskAnswers);
+});
+
+function fixSelectedOptionsInDropdown(taskId,taskAnswers) {
+  $(taskHandlerIdPrefix + taskId + ' .panel-body .choice-wrap').each(function () {
+    const valueB = $(this).find('input.side-b').val()
+
+    const choiceIsEmpty = (valueB === '');
+
+    $(this).find('input.side-b').val(choiceIsEmpty ? '' : valueB);
+    $(this).find('button.choice-button .text-overflow').text(choiceIsEmpty ? 'Option' : valueB)
+    if (choiceIsEmpty) {
+      $(this).find('button.choice-button').removeClass('btn-primary').addClass('btn-default')
+    } else {
+      $(this).find('button.choice-button').removeClass('btn-default').addClass('btn-primary')
+    }
+
+    const selected = Object.values(taskAnswers);
+    $(this).find('.choice-side-b').removeClass('active').removeClass('selected').each(function () {
+      const choiceB = $(this).find('a').text();
+      if (valueB === choiceB) {
+        $(this).addClass('active')
+      } else if (selected.indexOf(choiceB) > -1) {
+        $(this).addClass('selected')
+      }
+    })
+  })
+}
--- resources/views/includes/preview/segments/task_types/correspondence.blade.php
+++ resources/views/includes/preview/segments/task_types/correspondence.blade.php
@@ -1,51 +1,41 @@
-<div class="panel panel-default task-wrap relative" data-task-type="correspondence" data-task-id="{{$task->id}}">
-  <?php
-    $sides = ['a'=>[],'b'=>[]];
-  ?>
-  @foreach($task->correspondence as $cor)
-  <?php
-    $sides['a'][] = $cor->side_a;
-    $sides['b'][] = $cor->side_b;
-  ?>
-  @endforeach
-  <?php shuffle($sides['b']);shuffle($sides['a']); ?>
-  <div class="panel-heading">{{$task->description}} <br><br>
-    @foreach($sides['b'] as $b)
-      <a href="#" class="btn btn-info">
-        {{$b}}
-      </a>
-    @endforeach
-    @include('includes.preview.segments.task_points', ['total' => $task->points])
-  </div>
-  <div class="panel-body">
-    @foreach($sides['a'] as $a)
-      <div class="col-xs-8 row-margin-bottom">
-        <a href="#" class="btn btn-default col-xs-8">
-          {{$a}}
-        </a>
-      </div>
-      <div class="col-xs-4 row-margin-bottom">
-        <a href="#" class="btn btn-default btn-dotted">Drag here</a>
-      </div>
-    @endforeach
-  </div>
-</div>
+@php
+    //todo move all these to the controller
+    $sides = ['a' => [], 'b' => []];
+    foreach($task->correspondence as $cor){
+        $sides['a'][] = $cor->side_a;
+        $sides['b'][] = $cor->side_b;
+    }
+    shuffle($sides['b']);
+    shuffle($sides['a']);
 
-<script>
-//https://jqueryui.com/droppable/
-  function ReorderSegmentTasks(){
-    $("#segment-body .task-wrap").each(function(index) {
-      $(this).find('.order-wrap .order-value').text(index+1)
-    })
-    $(document).find(".task-wrap .panel-body .task-list").sortable({
-      appendTo: document.body,
-      cursor: "move",
-      items: "> .task-choice",
-      placeholder: "choice-placeholder",
-      opacity: 0.5,
-      handle: '.choice-handle',
-      update: function( event, ui ) {}
-    });
-  }
+    $answers = [];
+    foreach($sides['a'] as $a){
+        $answers[$a] = null;
+    }
+    //todo make the answers prefilled
 
-</script>
+@endphp
+
+@foreach($sides['a'] as $a)
+    <div class="col-xs-12 row-margin-bottom choice-wrap">
+        <div class="input-group col-xs-12 pull-left">
+            <input type="hidden" class="form-control side-a text-wrap" value="{{$a}}">
+            <input type="hidden" class="form-control side-b text-wrap" value="{{$answers[$a]}}">
+            <span class="form-control text-wrap choice-side-a">{{$a}}</span>
+            <span class="input-group-addon"><i class="fa fa-hand-o-right"></i></span>
+            <div class="input-group-btn">
+                <button type="button" class="btn btn-default dropdown-toggle choice-button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                    <span class="no-padding col-xs-10 text-overflow">Option</span> <span class="caret"></span>
+                    <span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right">
+                    @foreach($sides['b'] as $b)
+                        <li class="choice-side-b"><a href="#">{{$b}}</a></li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
+@endforeach
