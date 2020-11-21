cf9df8a02293444afeef1cc9a331af0fac446102
Duke Vomvyras <dukevomv@gmail.com>
2020-11-22 01:50:54 +0200

    [fix] Correct drop of foreign key in migration

--- database/migrations/2019_01_29_185003_add_columns_in_test_table.php
+++ database/migrations/2019_01_29_185003_add_columns_in_test_table.php
@@ -15,11 +15,11 @@ class AddColumnsInTestTable extends Migration
     {
         Schema::table('tests', function (Blueprint $table) {
             $table->integer('started_by_user')->unsigned()->nullable()->after('started_at');
-            $table->foreign('started_by_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('started_by_user')->references('id')->on('users')->onDelete('set null');
             $table->integer('finished_by_user')->unsigned()->nullable()->after('finished_at');
-            $table->foreign('finished_by_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('finished_by_user')->references('id')->on('users')->onDelete('set null');
             $table->integer('graded_by_user')->unsigned()->nullable()->after('graded_at');
-            $table->foreign('graded_by_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('graded_by_user')->references('id')->on('users')->onDelete('set null');
         });
     }
 
@@ -31,12 +31,12 @@ class AddColumnsInTestTable extends Migration
     public function down()
     {
         Schema::table('tests', function (Blueprint $table) {
+            $table->dropForeign(['started_by_user']);
             $table->dropcolumn('started_by_user');
-            $table->dropForeign('started_by_user');
+            $table->dropForeign(['finished_by_user']);
             $table->dropcolumn('finished_by_user');
-            $table->dropForeign('finished_by_user');
+            $table->dropForeign(['graded_by_user']);
             $table->dropcolumn('graded_by_user');
-            $table->dropForeign('graded_by_user');
         });
     }
 }
