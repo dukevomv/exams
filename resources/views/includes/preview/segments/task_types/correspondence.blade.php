363a2a8e0dd4457ace5af57c613e33391d6b0816
Duke Vomvyras <dukevomv@gmail.com>
2020-11-29 01:57:01 +0200

    [feat] Make correspondence student answers to show in professor what is right and wrong

--- resources/views/includes/preview/segments/task_types/correspondence.blade.php
+++ resources/views/includes/preview/segments/task_types/correspondence.blade.php
@@ -6,7 +6,7 @@
             <span class="form-control text-wrap choice-side-a">{{$a}}</span>
             <span class="input-group-addon"><i class="fa fa-hand-o-right"></i></span>
             <div class="input-group-btn">
-                <button type="button" class="btn btn-default dropdown-toggle choice-button"
+                <button type="button" class="btn @if(!isset($b['selected']) || !isset($b['correct'])) btn-default @elseif($b['selected'] == $b['correct']) btn-success @else btn-danger @endif dropdown-toggle choice-button"
                         data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
                     <span class="no-padding col-xs-10 text-overflow">Option</span> <span class="caret"></span>
@@ -14,7 +14,7 @@
                 </button>
                 <ul class="dropdown-menu dropdown-menu-right">
                     @foreach($b['available'] as $avail)
-                        <li class="choice-side-b"><a href="#">{{$avail}}</a></li>
+                        <li class="choice-side-b @if(isset($b['correct']) && $b['correct'] == $avail) correct @endif"><a href="#">{{$avail}}</a></li>
                     @endforeach
                 </ul>
             </div>
