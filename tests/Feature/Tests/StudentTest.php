d805352012eebd8bf145e202e1398617cc5ba138
Duke Vomvyras <dukevomv@gmail.com>
2021-01-24 18:10:55 +0200

    [test] Implement some Student Test tests

--- tests/Feature/Tests/StudentTest.php
+++ tests/Feature/Tests/StudentTest.php
@@ -2,34 +2,69 @@
 
 namespace Tests\Feature\Tests;
 
+use App\Enums\TaskType;
+use App\Enums\UserRole;
+use App\Models\Segments\Task;
 use App\Models\User;
+use Tests\Builders\LessonBuilder;
+use Tests\Builders\TestBuilder;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class StudentTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testDummy()
-    {
-        $user = factory(User::class)->create();
-        echo $user->role;
-        $response = $this->get('/');
+class StudentTest extends TestCase {
 
+    private $user;
+    private $lesson;
+
+    protected function setUp() {
+        parent::setUp();
+
+        $this->user = factory(User::class)->states([UserRole::STUDENT])->create();
+        $this->lesson = LessonBuilder::instance()->withUser($this->user->id)->build();
+        $this->actingAs($this->user);
+    }
+
+    public function testCanLoadTestPreviewPage() {
+        $test = TestBuilder::instance()
+                           ->inLesson($this->lesson->id)
+                           ->published()
+                           ->withUser($this->user->id)
+                           ->build();
+        $response = $this->get('/tests/' . $test->id);
+        $response->assertStatus(200);
+    }
+    public function testCanRegisterOnTest() {}
+    public function testCanPreviewStartedTest() {
+        $test = TestBuilder::instance()
+                           ->inLesson($this->lesson->id)
+                           ->withSegmentTasks([['type' => TaskType::CMC, 'options' => 3]])
+                           ->withSegmentTasks([['type' => TaskType::RMC, 'options' => 3]])
+                           ->withSegmentTasks([['type' => TaskType::FREE_TEXT]])
+                           ->started()
+                           ->withUser($this->user->id)
+                           ->build();
+        $response = $this->get('/tests/' . $test->id);
         $response->assertStatus(200);
     }
 
-    public function testStudentCanNotSubmitOnDraftStatus() {}
-    public function testStudentCanNotSubmitOnPublishedStatus() {}
-    public function testStudentCanDraftOnStartedStatus() {}
-    public function testStudentCanSubmitOnStartedStatus() {}
-    public function testStudentCanDraftOnFinishedStatusInPreparationSeconds() {}
-    public function testStudentCanSubmitOnFinishedStatusInPreparationSeconds() {}
-    public function testStudentCanNotSubmitOnFinishedStatus() {}
+    public function testCanNotSubmitOnDraftStatus() {
+    }
+
+    public function testCanNotSubmitOnPublishedStatus() {
+    }
+
+    public function testCanDraftOnStartedStatus() {
+    }
+
+    public function testCanSubmitOnStartedStatus() {
+    }
+
+    public function testCanDraftOnFinishedStatusInPreparationSeconds() {
+    }
+
+    public function testCanSubmitOnFinishedStatusInPreparationSeconds() {
+    }
+
+    public function testCanNotSubmitOnFinishedStatus() {
+    }
 
 }
