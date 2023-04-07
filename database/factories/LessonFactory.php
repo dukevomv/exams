71cb4f0f6ea1eef7d0741b1300be7ad54f3cc882
Duke Vomvyras <dukevomv@gmail.com>
2023-04-07 17:10:01 +0300

    [test] Update lesson builder tto create from list of courses

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -10,25 +10,11 @@
 | database. Just tell the factory how a default model should look.
 |
 */
-const SUBJECTS = ['Accounting and Finance', 'Aeronautical and Manufacturing Engineering', 'Agriculture and Forestry',
-    'Anatomy and Physiology', 'Anthropology', 'Archaeology', 'Architecture', 'Art and Design', 'Biological Sciences',
-    'Building', 'Business and Management Studies', 'Chemical Engineering', 'Chemistry', 'Civil Engineering',
-    'Classics and Ancient History', 'Communication and Media Studies', 'Complementary Medicine', 'Computer Science',
-    'Counselling', 'Creative Writing', 'Criminology', 'Dentistry', 'Drama Dance and Cinematics', 'Economics',
-    'Education', 'Electrical and Electronic Engineering', 'English', 'Fashion', 'Film Making', 'Food Science',
-    'Forensic Science', 'General Engineering', 'Geography and Environmental Sciences', 'Geology',
-    'Health And Social Care', 'History', 'History of Art Architecture and Design',
-    'Hospitality Leisure Recreation and Tourism', 'Information Technology', 'Land and Property Management', 'Law',
-    'Linguistics', 'Marketing', 'Materials Technology', 'Mathematics', 'Mechanical Engineering', 'Medical Technology',
-    'Medicine', 'Music', 'Nursing', 'Occupational Therapy', 'Pharmacology and Pharmacy', 'Philosophy',
-    'Physics and Astronomy', 'Physiotherapy', 'Politics', 'Psychology', 'Robotics', 'Social Policy', 'Social Work',
-    'Sociology', 'Sports Science', 'Veterinary Medicine', 'Youth Work',
-];
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\Models\Lesson::class, function (Faker\Generator $faker) {
     return [
-        'name'       => $faker->randomElement(SUBJECTS),
+        'name'       => $faker->randomElement(\Tests\Builders\LessonBuilder::SUBJECTS),
         'semester'   => $faker->randomDigitNot(0),
         'gunet_code' => str_random(10),
     ];
--- resources/views/includes/trial-jumbotron.blade.php
+++ resources/views/includes/trial-jumbotron.blade.php
@@ -1,5 +1,5 @@
 @if(config('app.demo.enabled') && !Session::has('demo'))
-    <div class="jumbotron">
+    <div class="jumbotron hidden">
         <h1>Trial Mode</h1>
         <p>Perform a live examination online. <br>Create a test for your class to take, and get <b>instant results</b>.</p>
         <ul class="text-lg">
@@ -12,7 +12,7 @@
             <li>Create an email list with your students so you can invite them.</li>
             <li>By saving the test as published, you will allow your invite list to subscribe to your test, and participate</li>
             <li>After the test has finished you will be able to grade the results and get a full report email.</li>
-            <li>We will also send an email to each student with their results.</li>
+            <li>We will also email each student with their results.</li>
             <li>After 30 days, this account and it's data will be lost forever.</li>
         </ul>
         <hr>
@@ -62,5 +62,86 @@
         </form>
         <br>
         </p>
+    </div>
+
+    <style>
+        .trial-demo-wrap{
+            margin-top: 30px;
+        }
+        .trial-details-wrap .list-wrap img{
+            width:20px;
+            margin-right:10px;
+        }
+        .trial-login-wrap{
+            background-color: #FFF2C7;
+            margin-top:50px;
+            padding-top:30px;
+            padding-bottom: 52px;
+        }
+        .demo-wrap{
+            background-color: #EBE5FF;
+            padding-top:30px;
+            padding-bottom: 52px;
+        }
+        .demo-wrap a{
+            color:#3B00FF;
+        }
+        .trial-login-wrap a{
+            color:#3c4ee4;
+        }
+        .demo-wrap .image-wrap{
+            position: relative;
+        }
+        .trial-login-wrap .image-wrap img{
+            position: absolute;
+            right:60px;
+            top:-60px;
+            width: 30%;
+        }
+        .demo-wrap .image-wrap img{
+            position: absolute;
+            right:0px;
+            bottom:-80px;
+            width: 25%;
+        }
+        .jumbotron p{
+            font-size:16px;
+        }
+    </style>
+    <div class="col-xs-12 trial-demo-wrap">
+        <div class="row">
+            <div class="col-xs-12 col-md-6">
+                <div class="jumbotron demo-wrap">
+                    <div class="row">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <h3>See how it works</h3>
+                                <p>Create auto-generated data and explore our features using different roles like <b>Admin</b>, <b>Professor</b> and <b>Student</b>. Manage Exams, Courses and Users in a sandbox environment to <b>understand the Exams Studio's capabilities</b>.</p>
+                            </div>
+                        </div>
+                        <a href="https://demo.exams.studio">Explore Demo <i class="fa fa-arrow-right"></i></a>
+                    </div>
+                    <div class="image-wrap">
+                        <img width="150px" src="{{asset('images/stars@2x.png')}}" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6">
+                <div class="jumbotron trial-login-wrap">
+                    <div class="row">
+                        <div class="row">
+                            <div class="col-xs-8">
+                                <h3>Generate Trial Exam</h3>
+                                <p>Perform a real examination with student invites on a future date.<br>You will need to generate the Exam questions after setup.</p>
+                                <a href="{{url('trial')}}">Setup Trial Exam <i class="fa fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                        <div class="image-wrap">
+                            <img width="100px" src="{{asset('images/professor.png')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endif
